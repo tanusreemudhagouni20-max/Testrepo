c3662aeae8e721715a7fbb506ba07d2850ab4d87
tanusreemudhagouni20-max <tanusreemudhagouni20@gmail.com>
2026-03-04 20:06:13 +0530

    Add files via upload

---
+++ Tanu covid sql query 1.sql
@@ -0,0 +1,7 @@
+SELECT TOP (1000) [continent]
+      ,[location]
+      ,[date]
+      ,[population]
+      ,[new_vaccinations]
+      ,[Rollingvaccinations]
+  FROM [tanu_portfolio_project].[dbo].[PercentPopulationVaccinaated]
